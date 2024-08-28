84c1acc8ad31186fa203c72dd920f45999c1d4f8
rashik007 <rashikarmacharya@gmail.com>
2024-08-28 17:52:45 +0545

    added second coming soon screen on template_2.html

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let countDownDate = new Date("August 30, 2024 15:00:00").getTime();
 
+
 let setInterval_custom_date = setInterval(function() {
     let now = new Date().getTime();
     let distance = countDownDate - now;
@@ -9,13 +10,79 @@ let setInterval_custom_date = setInterval(function() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById('countDown').innerHTML = 
-    
-    days + "d: " + hours + "h: "
-    + minutes + "m: " + seconds + "s ";
+    if(document.getElementById('countDown')) {
 
-    //write a text when countdown is finishied
-    if(distance < 0) {
-        document.getElementById('countDown').innerHTML = "EXPIRED";
+      document.getElementById('countDown').innerHTML = 
+      
+      days + "d: " + hours + "h: "
+      + minutes + "m: " + seconds + "s ";
+  
+      //write a text when countdown is finishied
+      if(distance < 0) {
+          document.getElementById('countDown').innerHTML = "EXPIRED";
+      }
+    }
+
+
+    if(document.getElementById('countDown__2')) {
+
+      document.getElementById('countDown__2').innerHTML = 
+          `
+          <div class="flex flex-col items-center justify-center">
+            <p class="">${days}</p>
+            <p class="type__text">days</p>
+          </div>
+          <div class="flex flex-col  items-center justify-center">
+            <p class="">${hours}</p>
+            <p class="type__text">hours</p>
+          </div>
+          <div class="flex flex-col  items-center justify-center">
+            <p class="">${minutes}</p>
+            <p class="type__text">minutes</p>
+          </div>
+          <div class="flex flex-col  items-center justify-center">
+            <p class="">${seconds}</p>
+            <p class="type__text">seconds</p>
+          </div>
+          `
+  
+      //write a text when countdown is finishied
+      if(distance < 0) {
+          document.getElementById('countDown__2').innerHTML = "EXPIRED";
+      }
     }
 }, 1000);
+
+
+
+// http://ahrengot.com/tutorials/greensock-javascript-animation
+
+var $cursor = $('.cursor');
+
+function moveCursor(e) {
+  $cursor.addClass('is-moving');
+  
+  TweenLite.to($cursor, 0.23, {
+    left: e.pageX,
+    top: e.pageY,
+    ease: Power4.easOut
+  });
+  
+  clearTimeout(timer);
+
+   var timer = setTimeout(function() {
+       $cursor.removeClass('is-moving');
+   }, 300);
+}
+
+$(window).on('mousemove', moveCursor);
+
+
+/*************************
+ *  video playback
+ * ****************************
+ */
+var video = document.getElementById('video__m');
+
+    // Set the playback rate to slow down the video
+video.playbackRate = 0.5; 
